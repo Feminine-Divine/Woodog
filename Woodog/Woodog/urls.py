3e2146fcb14493e297045b6a5559897d72d45c9c
Khushi <dpskhu13108@gmail.com>
2021-05-12 20:22:59 +0530

    added

--- Woodog/Woodog/urls.py
+++ Woodog/Woodog/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls.conf import include
 
 urlpatterns = [
+    path('',include('woodogdata.urls')),
     path('admin/', admin.site.urls),
 ]
