faa39eb8d32ab164b3cecc5f757a26d997c39d12
Khushi <dpskhu13108@gmail.com>
2021-05-14 00:19:12 +0530

    added

--- Woodog/authentication/urls.py
+++ Woodog/authentication/urls.py
@@ -1,7 +1,9 @@
 from django.utils import regex_helper
-from .views import RegistrationView
+from .views import RegistrationView, UsernameValidationView
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-    path('register',RegistrationView.as_view(), name='register')
+    path('register',RegistrationView.as_view(), name='register'),
+    path("validate-username", csrf_exempt(UsernameValidationView.as_view()), name="validate-username"),
 ]
--- Woodog/authentication/views.py
+++ Woodog/authentication/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.views import View
+from django.http import JsonResponse
+from django.contrib.auth.models import User
+import json
 
 # Create your views here.
+
+class UsernameValidationView(View):
+    def get(self, request):
+        data = json.loads(request.body)
+        username = data['username']
+        if not str(username).isalnum():
+            return JsonResponse({'username_error': 'Username should only contain alphanumeric characters'})
+        return JsonResponse({'username_valid': True})
 
 class RegistrationView(View):
     def get(self, request):
