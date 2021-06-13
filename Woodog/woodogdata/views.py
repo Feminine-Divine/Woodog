420045a7bac4c33d7095061de00bf349bfddc5d7
Saup21 <sauparna.gupta@gmail.com>
2021-06-14 00:55:17 +0530

    BT

--- Woodog/woodogdata/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required
-
-# Create your views here.
-
-# @login_required(login_url='/authentication/login')
-def index(request):
-    return render(request,'woodogdata/index.html')
