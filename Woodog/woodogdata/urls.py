6c84b2b58dc3967dc75dfe3bcc9290395647cc48
Jayapritha21 <jaya9282@outlook.com>
2021-06-15 15:39:16 +0530

    created login page

--- Woodog/woodogdata/urls.py
+++ Woodog/woodogdata/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='woodogdata')
+    path('', views.index, name='woodogdata'),
+    path('login/', views.login, name='woodogdata'),
 ]
