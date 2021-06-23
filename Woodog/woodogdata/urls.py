d38974bc6cc0e9e4aff719549ea761b05b28090e
Jayapritha N <53329034+coding-geek21@users.noreply.github.com>
2021-06-23 17:39:01 +0530

    enhanced the contact page (#92)

--- Woodog/woodogdata/urls.py
+++ Woodog/woodogdata/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='woodogdata'),
     path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
 ]
