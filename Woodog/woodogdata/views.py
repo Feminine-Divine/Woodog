010073a94a33a5391133dd6dc234a7b760072354
Muhammad Abdullah <abdullahquddus7@gmail.com>
2021-06-23 17:11:20 +0500

    Suggestions updated (#87)
    Co-authored-by: Khushi Gautam <65439761+khushishikhu@users.noreply.github.com>

--- Woodog/woodogdata/views.py
+++ Woodog/woodogdata/views.py
@@ -1,14 +1,27 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
 
 # Create your views here.
 
 # @login_required(login_url='/authentication/login')
+
 def index(request):
-    return render(request,'woodogdata/index.html')
+    crr_user = request.user
+    try : 
+        crr_user = User.objects.get(username = crr_user)
+        if crr_user.is_active : 
+            params = {'success' : True , 'name' : crr_user}
+        else : 
+            params = {'success' : False , 'name' : crr_user}
+        return render(request, 'woodogdata/index.html' , params )
+    except : 
+        return render(request, 'woodogdata/index.html' )
+
 
 def about(request):
     return render(request,'woodogdata/about.html')
 
 def contact(request):
     return render(request,'woodogdata/contact.html')
+
