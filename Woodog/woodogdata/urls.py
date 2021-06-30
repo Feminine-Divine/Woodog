78ac8fd96d15af225a0afd32dd62129a536c2941
Your Name <7saikat7.com>
2021-06-30 22:24:26 +0530

    final contact form from website is working

--- Woodog/woodogdata/urls.py
+++ Woodog/woodogdata/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='woodogdata'),
     path('contact/',views.contact_us,name='contact_us'),
+    path('contact_usw/',views.contat_us_throughwebsite,name='contact_us_throughwebsite'),
 ]
--- Woodog/woodogdata/views.py
+++ Woodog/woodogdata/views.py
@@ -1,4 +1,5 @@
-from django.http.response import HttpResponse
+import re
+from django.http.response import HttpResponse, JsonResponse
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from rest_framework import serializers
@@ -18,9 +19,10 @@ def index(request):
 @csrf_exempt
 def contact_us(request): 
  if request.method=='POST':
-  
   try:
     complex_data=request.body
+    print(complex_data)
+    print(type(complex_data))
     print("got the first print !")
     stream_data=io.BytesIO(complex_data)
     print(stream_data)
@@ -30,11 +32,11 @@ def contact_us(request):
     print(serialization_ofdata)
     print("bes eto ta tokkoi !")
     if serialization_ofdata.is_valid():
-      serialization_ofdata.save()
-      print("process done succesfully.........!")
-      msg={'done_msg':'Data has been sucessfully created and submitted!'}
-      json_msg=JSONRenderer().render(msg)
-      return render(request,'woodogdata/contact_us.html',{'msg':json_msg})
+        serialization_ofdata.save()
+        print("process done succesfully.........!")
+        msg={'done_msg':'Data has been sucessfully created and submitted!'}
+        json_msg=JSONRenderer().render(msg)
+        return render(request,'woodogdata/contact_us.html',{'msg':json_msg})
     else:
         error_msg=JSONRenderer().render(serialization_ofdata.errors)
         print(error_msg)
@@ -42,13 +44,47 @@ def contact_us(request):
         msg={'done_msg':'Error Occure During handeling data '}
         json_msg=JSONRenderer().render(msg)
         return HttpResponse('not validated !')
-        return render(request,'woodogdata/contact_us.html',{'msg':json_msg})
   except:
-   forms=ContactForm(request.post)
-   if forms.is_valid():
-    form.save()
-    return redirect('/')
+    return HttpResponse("Not submitted properly !")
 
  else:  
     forms=ContactForm()
     return render(request,'woodogdata/contact_us.html',{'form':forms})
+
+@csrf_exempt
+def contat_us_throughwebsite(request):
+  if request.method =='GET':
+    return render(request,'woodogdata/contact_us.html')
+    return HttpResponse("done!")
+
+  else:
+   try:
+    json_body=request.body
+    print(json_body)
+    print('the input data !')
+    body=io.BytesIO(json_body)
+    print(body)
+    python_data=JSONParser().parse(body)
+    print(python_data)
+    print('Done!')
+    serialization_ofdata=Contact_form_se(data=python_data)
+    print(serialization_ofdata)
+    print("Form fo intra sharing is working !")
+    if serialization_ofdata.is_valid():
+        serialization_ofdata.save()
+        print("process done succesfully.........!")
+        resp={'response':contatct_form_submitted}
+        #return JsonResponse(resp)
+        return render(request,'woodogdata/contact_us.html' ,{'msg22':'submitted succesfully !'})
+
+       
+    else:
+        error_msg=JSONRenderer().render(serialization_ofdata.errors)
+        print(error_msg)
+        print('data is not validated !')
+        msg={'done_msg':'Error Occure During handeling data '}
+        json_msg=JSONRenderer().render(msg)
+        return HttpResponse('not validated !')
+   except:
+        return HttpResponse("Not submitted properly !")
+  
