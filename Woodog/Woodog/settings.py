22eef6a82726fc6b66cbac27f17588c6f1cc54f6
Jayapritha21 <jaya9282@outlook.com>
2021-07-07 21:32:52 +0530

    added forgot password functionality

--- Woodog/Woodog/settings.py
+++ Woodog/Woodog/settings.py
@@ -145,6 +145,7 @@ MESSAGE_TAGS = {
 }
 
 #email stuff
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'femininedivine591@gmail.com'
 EMAIL_USE_TLS = True
