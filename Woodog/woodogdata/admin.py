d0eef3437ec60904d8195e470178fa5388134357
Hari Ram J P <58631762+hariramjp777@users.noreply.github.com>
2021-07-10 11:58:05 +0530

    Revert "Rest Api in contact us  (#109)" (#136)
    This reverts commit 337f79e5530d82d8bce72646a09d2fd6f54cf0db.

--- Woodog/woodogdata/admin.py
+++ Woodog/woodogdata/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import Seeker_Post, Comment, ContactModel
+from .models import Seeker_Post, Comment
 
 # Register your models here.
 admin.site.register(Seeker_Post)
 admin.site.register(Comment)
-admin.site.register(ContactModel)
