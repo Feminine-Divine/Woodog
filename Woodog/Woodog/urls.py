22a39c87c93f116fb78d8ae03da99cd8f3b2757a
Jayapritha N <53329034+coding-geek21@users.noreply.github.com>
2021-07-18 21:35:40 +0530

    Added Gallery model (#143)
    * added gallery model
    
    * added gallery page model

--- Woodog/Woodog/urls.py
+++ Woodog/Woodog/urls.py
@@ -17,6 +17,8 @@ from django import urls
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('',include('woodogdata.urls')),
@@ -24,3 +26,6 @@ urlpatterns = [
     path('dashboard/',include('appdashboard.urls')),
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
